bc222f4f458a927780443654d91d423688273797
Galih Arghubi <galiharghubi@gmail.com>
2025-04-12 13:27:59 +0700

    fix: invest amount calculation

--- internal/application/loan/invest.go
+++ internal/application/loan/invest.go
@@ -29,19 +29,19 @@ func (u *Usecase) Invest(ctx context.Context, req *dto.InvestRequest) helper.JSO
 		return helper.ResponseFailed(http.StatusNotFound, "Investor not found", nil)
 	}
 
-	if loanData.TotalInvested+req.Amount > loanData.TotalAmount {
+	if loanData.TotalInvested+req.Amount > loanData.PrincipalAmount {
 		l.Error("investment amount exceeds loan amount")
 		return helper.ResponseFailed(http.StatusBadRequest, "Investment amount exceeds loan amount", nil)
 	}
 
 	loanData.TotalInvested += req.Amount
-	if loanData.TotalAmount == loanData.TotalInvested {
+	if loanData.PrincipalAmount == loanData.TotalInvested {
 		loanData.Status = "ready_to_disbursed"
 	}
 
 	timestamp := time.Now().Format("20060102150405")
 	text := fmt.Sprintf("Dear Mr/Mrs. %s, Your money successfully invested on loan %d,\nplease read this agreement for details.", investorData.Name, *loanData.ID)
-	savePath := fmt.Sprintf("./uploads/%s_%d_%d_%s.pdf", timestamp, *loanData.ID, *req.InvestorID, "agreement_letter")
+	savePath := fmt.Sprintf("./uploads/%s_%d_%d_%s_%s.pdf", timestamp, *loanData.ID, *req.InvestorID, "invest", "agreement_letter")
 	err = pdf.GeneratePDF(text, savePath)
 	if err != nil {
 		l.Error("failed to generate agreement letter", zap.Error(err))
